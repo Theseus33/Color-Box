826c25ca6cb649b04f07038b67d388f59e7e02f1
Rana <ranab33@gmail.com>
2020-08-17 20:01:24 -0400

    add styling to colorbox

--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 
 class Palette extends Component {
     render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background={color.color} name={color.name} />
+            ));
         return (
-            <div>
+            <div className="Palette">
                 <h1>Palette</h1>
                 {/*Navbar goes here*/}
-                <div className="Palette-colors">
+                <div className="Palette-colors">{colorBoxes}</div>
+                    
                     {/*color boxes component*/}
-                </div>
+                
                 {/* footer */}
             </div>
         );
