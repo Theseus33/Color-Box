6566a51c69e395ddd30ec1cd162b20a90bd50fc2
Rana <ranab33@gmail.com>
2020-08-19 20:44:55 -0400

    add chroma.js and shades of colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Palette from './Palette.js';
 import seedColors from './seedColors';
+import { generatePalette } from "./colorHelper";
 
 
 function App() {
   return (
+    console.log(generatePalette);
     <div className="App">
       <Palette {...seedColors[4]} />
     </div>
