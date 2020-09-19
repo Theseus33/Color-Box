e538a1ef4df42556a71ced76647f1402c1a3b5fc
Rana <ranab33@gmail.com>
2020-09-18 23:22:16 -0400

    add snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
@@ -9,7 +12,7 @@ import "./Navbar.css";
 class Navbar extends Component {
     constructor(props){
         super(props);
-        this.state = { format: "hex" };
+        this.state = { format: "hex", open: "true" };
         this.handleChange = this.handleChange.bind(this);
     }
     handleChange(e){
@@ -41,6 +44,22 @@ class Navbar extends Component {
                     <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
                 </Select>
             </div>
+            <Snackbar 
+            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+            open={this.state.open}
+            autoHideDuration={3000}
+            message={<span id="message-id">Format Changed!</span>}
+            ContentProps={{
+            "aria-describedby": "message-id"
+            }}
+            onClose={this.closeSnackbar}
+            action={[
+            <IconButton onCLick={this.closeSnackbar} color="inherit" key="close" aria-label="close">
+                <CloseIcon/>
+            </IconButton>
+
+            ]}
+            />
             </header>
 
         );
